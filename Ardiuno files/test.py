93092192449fc95a42f1ccbc58384a2a7bbcb80c
qhk895 <kpsburhenne@gmail.com>
2022-09-07 11:23:43 +0200

    circle

--- Ardiuno files/test.py
+++ Ardiuno files/test.py
@@ -3,7 +3,34 @@ from time import sleep
 
 arlo = robot.Robot()
 
-print(arlo.go_diff(64,64,1,1))
+# print(arlo.go_diff(64,64,1,1))
+# sleep(2)
+# print(arlo.stop)
+# print("finish")
+
+print(arlo.go_diff(64,64,1,-1))
 sleep(2)
 print(arlo.stop)
-print("finish")
+
+
+# #Rotation?
+# print(arlo.go_diff(64,64,1,-1))
+# sleep(2)
+# print(arlo.stop)
+
+# #Square?
+# print(arlo.go_diff(64,64,1,1)) #drive straigt: Start
+# sleep(2)
+# print(arlo.go_diff(64,64,1,-1)) #turn 1
+# sleep(2)
+# print(arlo.go_diff(64,64,1,1))#drive straigt
+# sleep(2)
+# print(arlo.go_diff(64,64,1,-1))#turn 2
+# sleep(2)
+# print(arlo.go_diff(64,64,1,1))#drive straigt
+# sleep(2)
+# print(arlo.go_diff(64,64,1,-1))#turn 3
+# sleep(2)
+# print(arlo.go_diff(64,64,1,1))#drive straigt: Stop
+# sleep(2)
+# print(arlo.stop)
